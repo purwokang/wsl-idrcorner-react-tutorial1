c3e6b474d48a18e6f1fdc0ddd147eec1344caaa7
purwokang <purwo.com@gmail.com>
2020-12-10 14:53:34 +0700

    menghapus beberapa file dan menambah file produk.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Produk from './Produk'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hallo Aplikasi React Perdanaku</h1>
+        <Produk />
+        <br />
+        <Produk />
+        <br />
+        <Produk />
+        <br />
+        <Produk />
+        <br />
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/Produk.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Produk extends Component {
+    render() {
+        return (
+            <div className='kotak'>
+                <p>Nama Produk: Monitor</p>
+                <p>Harga: 2000000</p>
+                <p>Stok: 70</p>
+            </div>
+        )
+    }
+}
+
+export default Produk;
