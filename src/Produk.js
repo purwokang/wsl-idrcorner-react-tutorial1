e4ae717f6bba6c490634b89610eff0e4ed6a336b
purwokang <purwo.com@gmail.com>
2020-12-12 20:35:27 +0700

    conditional rendering 2

--- src/Produk.js
+++ src/Produk.js
@@ -2,28 +2,20 @@ import React from 'react'; // cukup import React
 
 const Produk = ({ produkProduk }) => {
 
-    // const { produkProduk } = props 
-    // langsugn akses ke produkProduk, tanpa perlu ditampung dulu ke variable props
-    // hasil sama, hanya penulisan code lebih singkat
-
-    // bekerja dengan react akan banyak menggunakan kombinasi dari container component dan ui (fungsioinal) component
-
     const listProduk = produkProduk.map(produk => {
 
-        if (produk.stok > 30) {
-            return (
-                <div className="card kotak">
-                    <div className='card-body' key={produk.id} >
-                        <h5 className='card-title' >Nama Produk: {produk.namaProduk}</h5>
-                        <p>Harga: {produk.harga}</p>
-                        <p>Stok: {produk.stok}</p>
-                    </div>
+        const list = produk.stok > 30 ? (
+            <div className="card kotak">
+                <div className='card-body' key={produk.id} >
+                    <h5 className='card-title' >Nama Produk: {produk.namaProduk}</h5>
+                    <p>Harga: {produk.harga}</p>
+                    <p>Stok: {produk.stok}</p>
                 </div>
+            </div>
+        ) : (
+                null
             )
-        } else {
-            return null;
-        }
-
+        return list
     })
 
     return (
